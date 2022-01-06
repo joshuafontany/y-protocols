ecbb40c8c23588c4838d07472f5f0a09cbb4d900
Joshua Fontany <joshua.fontany@gmail.com>
2022-01-05 16:36:42 -0800

    permissionApproved handler

--- auth.js
+++ auth.js
@@ -4,6 +4,7 @@ import * as encoding from 'lib0/encoding'
 import * as decoding from 'lib0/decoding'
 
 export const messagePermissionDenied = 0
+export const messagePermissionApproved = 1
 
 /**
  * @param {encoding.Encoder} encoder
@@ -15,7 +16,22 @@ export const writePermissionDenied = (encoder, reason) => {
 }
 
 /**
+ * @param {encoding.Encoder} encoder
+ * @param {string} reason
+ */
+ export const writePermissionApproved = (encoder, reason) => {
+  encoding.writeVarUint(encoder, messagePermissionApproved)
+  encoding.writeVarString(encoder, reason)
+}
+
+/**
  * @callback PermissionDeniedHandler
+ * @param {any} y
+ * @param {string} reason
+ */
+
+/**
+ * @callback PermissionApprovedHandler
  * @param {any} y
  * @param {string} reason
  */
@@ -25,9 +41,11 @@ export const writePermissionDenied = (encoder, reason) => {
  * @param {decoding.Decoder} decoder
  * @param {Y.Doc} y
  * @param {PermissionDeniedHandler} permissionDeniedHandler
+ * @param {PermissionApprovedHandler} permissionApprovedHandler
  */
-export const readAuthMessage = (decoder, y, permissionDeniedHandler) => {
+export const readAuthMessage = (decoder, y, permissionDeniedHandler, permissionApprovedHandler) => {
   switch (decoding.readVarUint(decoder)) {
     case messagePermissionDenied: permissionDeniedHandler(y, decoding.readVarString(decoder))
+    case messagePermissionApproved: permissionApprovedHandler(y, decoding.readVarString(decoder))
   }
 }
